160f2ab6bace42a5524b62a9fcbc629ad87543ca
Kevin Piacentini <kevinpiac@gmail.com>
2018-04-19 23:13:22 +0200

    configure travis + some tests

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Express Guard
 
-Express Guard (express-guard) enable you to manage the requests made to your express server. It's built to be simple and has a powerful syntax. With Express Guard, you only have to define allowed Features (such as 'viewPosts', 'removePost'...) for different user Roles (such as 'admin', 'postOwner'). Then  When a request is made to your server, the middleware will check the corresponding access policy and return a result based on the user's permissions.
+Express Guard (express-guard) allows you to manage the requests made to your express server. It's built to be simple and has a powerful syntax. With Express Guard, you only have to define allowed Features (such as 'viewPosts', 'removePost'...) for different user Roles (such as 'admin', 'postOwner'). Then  When a request is made to your server, the middleware will check the corresponding access policy and return a result based on the user's permissions.
 
 ### WIP
--- test/guard.test.js
+++ test/guard.test.js
@@ -1,8 +1,34 @@
 const { expect } = require('chai');
-const { helloWorld } = require('../lib/guard');
+const guard = require('../lib');
 
-describe('Introducing tests', () => {
-  it('should return Hello World', () => {
-    expect(helloWorld()).to.equal('Hello world');
+describe('Roles', () => {
+  it('should add a role', () => {
+    const g = guard;
+    const roleName = 'testRole';
+    const features = ['feature1', 'features2', 'feature3'];
+    const func = () => false;
+    g.addRole(roleName, func, features);
+    expect(g.roles.testRole).to.be.a('Object');
+    expect(g.roles.testRole.name).to.equal(roleName);
+    expect(g.roles.testRole.func).to.equal(func);
+    expect(g.roles.testRole.features).to.equal(features);
+  });
+
+  it('should throw TypeError if roleName is not a string', () => {
+    const g = guard;
+    const func = () => true;
+    const array = ['feature1'];
+
+    expect(() => {
+      g.addRole(1, func, array);
+    }).to.throw(TypeError);
+
+    expect(() => {
+      g.addRole([], func, array);
+    }).to.throw(TypeError);
+
+    expect(() => {
+      g.addRole(func, func, array);
+    }).to.throw(TypeError);
   });
 });
