030dc5205595ed4d70728779774aed3b63d284c1
Kevin Piacentini <kevinpiac@gmail.com>
2018-04-22 20:31:01 +0200

    guard roles setter + tests + few changes

--- test/roleList/roleList.constructor.test.js
+++ test/roleList/roleList.constructor.test.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const RoleList = require('../../lib/RoleList');
+const Role = require('../../lib/Role');
 
 describe('test roleList constructor', () => {
   it('should return a RoleList instance', () => {
@@ -9,5 +10,13 @@ describe('test roleList constructor', () => {
   it('should init RoleList with default values', () => {
     const roleList = new RoleList();
     expect(roleList.$roles).to.deep.equal({});
+  });
+  it('should init RoleList with roles', () => {
+    const roleList = new RoleList([
+      new Role('role1'),
+      new Role('role2'),
+    ]);
+    expect(roleList.getRole('role1')).to.be.an.instanceof(Role);
+    expect(roleList.getRole('role2')).to.be.an.instanceof(Role);
   });
 });
