cf512cbb987ad92c04a1c260bf4849a585bb5c06
Kevin Piacentini <kevinpiac@gmail.com>
2018-05-05 21:54:50 +0200

    Add requireAny middleware + tests

--- test/guard/guard.constructor.test.js
+++ test/guard/guard.constructor.test.js
@@ -15,4 +15,8 @@ describe('test guard constructor', () => {
   it('should embed a Role class as static getter', () => {
     expect(Guard.Role.name).to.equal('Role');
   });
+  it('should have an errors static getter', () => {
+    expect(Guard.errors).to.be.an('object');
+    expect(Guard.errors.FORBIDDEN).to.be.a('string');
+  });
 });
