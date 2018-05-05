bbf10d8913d88ec524fbd7bc7dbf7ca855f2f64f
Kevin Piacentini <kevinpiac@gmail.com>
2018-05-05 18:27:59 +0200

    Save new test

--- test/roleList/roleList.rolesByAllowedFeatures.test.js
+++ test/roleList/roleList.rolesByAllowedFeatures.test.js
@@ -32,6 +32,18 @@ describe('test roleList rolesByAllowedFeatures', () => {
       role1,
     ]);
   });
+  it('should return an empty list without args', () => {
+    const roleList = new RoleList();
+    const role1 = new Role('role1', {
+      can: ['feature1', 'feature2'],
+    });
+    const role2 = new Role('role2', {
+      can: ['feature3', 'feature4'],
+    });
+    roleList.addRole(role1);
+    roleList.addRole(role2);
+    expect(roleList.getRolesByFeatures()).to.deep.equal([]);
+  });
   it('should throw Error INVALID_FEATURE_LIST', () => {
     const roleList = new RoleList();
     const role1 = new Role('role1', {
